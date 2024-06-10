6e065f0a1328bf2c43f3e2cc355076c55e860a75
s1d6p01nt7 <163605035+s1d6point7bugcrowd@users.noreply.github.com>
2024-06-10 15:05:48 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+IN_SCOPE_ENDPOINTS = {
+    'blockchain': 'https://example.com/blockchain',
+    'zklogin': 'https://example.com/zklogin',
+    'deepbook': 'https://example.com/deepbook'
+}
+
+CUSTOM_HEADER = {'X-Custom-Header': 'your-username'}
+
+LOG_FILE = 'vulnerability_test_log.txt'
+REPORT_FILE = 'vulnerability_report.txt'
